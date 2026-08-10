c4e4dc508d849f67ee6ccec4742d7934ce3c0cde
s7mnmgv2cm-afk <s7mnmgv2cm@privaterelay.appleid.com>
2026-08-10 23:26:36 +0800

    Update enrich_campsite.js

--- enrich_campsite.js
+++ enrich_campsite.js
@@ -165,7 +165,7 @@ async function analyzeReviewsWithGemini(campsiteName, rawReviews) {
     return generateFallbackProsCons(campsiteName);
   }
 
-  const candidateModels = ['gemini-3.1-flash-lite', 'gemini-3.5-flash', 'gemini-3.5-flash-lite',  'gemini-3.1-pro-preview', 'gemini-3-flash-preview', 'gemini-2.5-flash', 'gemini-2.5-flash-lite', 'gemini-3.6-flash'];
+  const candidateModels = ['gemini-3.1-flash-lite', 'gemini-3.5-flash', 'gemini-3.5-flash-lite',  'gemini-3.1-pro-preview', 'gemini-3-flash-preview', 'gemini-2.5-flash', 'gemini-2.5-flash-lite'];
 
   for (const modelName of candidateModels) {
     try {
